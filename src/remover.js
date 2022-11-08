136354e27040247dd2c2bc9e8209e129d720745d
nikitsch <nikitsch5@gmail.com>
2022-11-08 20:38:38 +0300

    feat: remove color from hash when deleting column

--- src/remover.js
+++ src/remover.js
@@ -1,9 +1,21 @@
 function deleteColumn(event) {
-  event.target.closest(".column")?.remove()
+  let selectedСolumn = event.target.closest(".column")
+  selectedСolumn?.remove()
   updateColumnsState()
 
+  let colorAddedPage = selectedСolumn.firstChild.textContent.substring(1)
+  let locationHash = document.location.hash
+  
+  let arrayColors = locationHash.replace(/[#-]/g, '').match(/.{1,6}/g)
+  let indexDeletedColor = arrayColors.indexOf(`${colorAddedPage}`)
+
+  indexDeletedColor == -1 ? arrayColors.splice(0, 1) : arrayColors.splice(indexDeletedColor, 1)
+
+  let newHash = '#' + arrayColors.join('-')
+
+  document.location.hash = newHash
+
   if (!columns.length) {
-    infoSpace.remove()
     document.body.append(createBasement());
   }
 }
